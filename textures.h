0e117ed480a3159aa9edc23e0876c1b864f5f3c9
Amaury Oubraim <wasilewskiamaury@gmail.com>
2026-03-27 13:00:09 +0100

    Add files via upload

---
+++ textures.h
@@ -0,0 +1,78 @@
+#ifndef TEXTURES_H
+#define TEXTURES_H
+
+#define T 32
+#define MENU 386
+
+extern int menu[MENU][MENU];
+extern int menu2[MENU][MENU];
+extern int choix_des_niveaux[MENU][MENU];
+extern int regles[MENU][MENU];
+extern int game_over[MENU][MENU];
+extern int game_succes[MENU][MENU];
+extern int niveau_bloque[MENU][MENU];
+extern int intro_niveau_1[MENU][MENU];
+extern int intro_niveau_2[MENU][MENU];
+extern int intro_niveau_3[MENU][MENU];
+extern int intro_niveau_4[MENU][MENU];
+extern int intro_niveau_5[MENU][MENU];
+
+
+// fruits
+extern int cerise[T][T];
+extern int citron[T][T];
+extern int fraise[T][T];
+extern int myrtille[T][T];
+extern int orange[T][T];
+extern int pasteque[T][T];
+extern int peche[T][T];
+extern int pomme[T][T];
+extern int barriere[T][T];
+
+// planetes
+
+extern int mercure[T][T];
+extern int venus[T][T];
+extern int terre[T][T];
+extern int mars[T][T];
+extern int jupiter[T][T];
+extern int saturne[T][T];
+extern int uranus[T][T];
+extern int trounoir[T][T];
+
+// animaux
+
+extern int lapin[T][T];
+extern int chat[T][T];
+extern int mouton[T][T];
+extern int rino[T][T];
+extern int elephant[T][T];
+extern int dauphin[T][T];
+extern int crabe[T][T];
+extern int abeille[T][T];
+extern int cloture[T][T];
+
+// ballons
+
+extern int foot[T][T];
+extern int tennis[T][T];
+extern int basket[T][T];
+extern int volley[T][T];
+extern int rugby[T][T];
+extern int baseball[T][T];
+extern int petanque[T][T];
+
+// Chiffres
+
+extern int zero[T][T];
+extern int un[T][T];
+extern int deux[T][T];
+extern int trois[T][T];
+extern int quatre[T][T];
+extern int cinq[T][T];
+extern int six[T][T];
+extern int sept[T][T];
+extern int huit[T][T];
+extern int neuf[T][T];
+extern int croix[T][T];
+#endif
