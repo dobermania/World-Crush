c44ec355b549007ec11b4a686e3e12ac03f83ae9
Amaury Oubraim <wasilewskiamaury@gmail.com>
2026-03-27 13:00:41 +0100

    Add files via upload

---
+++ affichage.h
@@ -0,0 +1,13 @@
+#ifndef AFFICHAGE_H
+#define AFFICHAGE_H
+
+#define T 32
+#define MENU 386
+
+extern int afficher_game_succes();
+extern int afficher_niveau_bloque();
+extern int afficher_game_over();
+extern int afficher_regles();
+extern int afficher_choix_des_niveaux();
+
+#endif
